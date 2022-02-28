8e444e733630a4d6ea0568db19e84f713e1288ca
WaceraMercyThird <waceraaamercy@gmail.com>
2022-02-28 15:39:08 +0300

    add file

--- src/main.kt
+++ src/main.kt
@@ -1,36 +1,22 @@
-
-// concatenate the String
-fun main(){
-    val statement = "The owner of this"
-    val item = "Book"
-    val action = "will get it tomorrow"
-
-    val sentence = statement+" "+item+" "+action
-    println(sentence)
-
-
-//    Char and String
-    val fruits = "banana"
-    val x = fruits[1].toString()+fruits[2]+fruits[3]//character with single cannot be concatenate only the string with double quote//
-    println(fruits[0])
-    println(fruits[1])
-    println(fruits[2])
-    println(fruits[3])
-    println(fruits[4]) // the individual letters it a single line
-    println(x)
-
-    //Built in Function instead of concatenate Character with is not the right syntax for kotlin language
-    var fruit ="banana"
-    var f =fruit.first()// These are built in function character
-    println(f)
-
-
-     val Fruits = "Mango"
-    val y = Fruits[0].toString()+Fruits[1]
-    println(y)
-
-
-
-
+fun main() {
+    employeeAvgNo()
+    builtIn()
+}
+// the total numberof employees for both google and amazon company
+fun employeeTotalNo(google:Int, amazon:Int):Int{
+    var totalNumber = google + amazon
+    return totalNumber
+}
+// the average number of employee from the total of two companies
+fun employeeAvgNo(){
+   var totalNumber = employeeTotalNo(27800000, 890000000)
+    var average =totalNumber/2
+    println(average)
+}
+fun builtIn(){
+    var names = "banana"
+    println("name"+" "+names[0].toString())
 
 }
+
+
